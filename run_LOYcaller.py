74877ad1b8f4a18ca9b8a36a91b16125571a4946
bjurinformatik <josefin.bjurling@igp.uu.se>
2023-03-16 14:04:16 +0100

    edited paths

--- run_LOYcaller.py
+++ run_LOYcaller.py
@@ -1,7 +1,7 @@
 import LOYcaller
 
-cl = LOYcaller.call_loy("/Volumes/Gottmik/UKBB/updated_files/sub_l2r_chrY.txt", "/Volumes/Gottmik/UKBB/updated_files/sub_l2r_chrX.txt", 
-"/Volumes/Gottmik/UKBB/updated_files/sub_famData.fam")
+cl = LOYcaller.call_loy("/path/to/file/sub_l2r_chrY.txt", "/path/to/file/sub_l2r_chrX.txt", 
+"/path/to/file/sub_famData.fam")
 cl.save_output("male_mlrr.txt")
 
 loyP = LOYcaller.mLRRYtoPer("male_mlrr.txt")
