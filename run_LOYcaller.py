bae29f23891c73ea9e950878a1d9dab646a2d019
bjurinformatik <josefin.bjurling@igp.uu.se>
2023-03-16 13:56:36 +0100

    added another module

--- run_LOYcaller.py
+++ run_LOYcaller.py
@@ -3,3 +3,6 @@ import LOYcaller
 cl = LOYcaller.call_loy("/Volumes/Gottmik/UKBB/updated_files/sub_l2r_chrY.txt", "/Volumes/Gottmik/UKBB/updated_files/sub_l2r_chrX.txt", 
 "/Volumes/Gottmik/UKBB/updated_files/sub_famData.fam")
 cl.save_output("male_mlrr.txt")
+
+loyP = LOYcaller.mLRRYtoPer("male_mlrr.txt")
+loyP.write_csv("male_mlrr_loyP.txt")
